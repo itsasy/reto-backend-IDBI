1bd6c638a68c5870a8e91caab85639cf030018d7
Alexis <ale.maldo097@gmail.com>
2024-08-30 18:33:35 -0500

    init

---
+++ app/Http/Controllers/Vouchers/Voucher/DeleteVoucherHandler.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Vouchers\Voucher;
+
+use App\Services\VoucherService;
+use Exception;
+
+class DeleteVoucherHandler
+{
+    public function __construct()
+    {
+
+    }
+
+    public function __invoke()
+    {
+
+    }
+}
---
+++ routes/v1/vouchers/auth.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\Vouchers\GetVouchersHandler;
+use App\Http\Controllers\Vouchers\StoreVouchersHandler;
+use App\Http\Controllers\Vouchers\Voucher\DeleteVoucherHandler;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('vouchers')->group(
+    function () {
+        Route::get('/', GetVouchersHandler::class);
+        Route::post('/', StoreVouchersHandler::class);
+    }
+);
